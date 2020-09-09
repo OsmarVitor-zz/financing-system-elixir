45bf475587ca9de63cbc27fc33ec684ddfb8ff6b
OsmarVitor <perez_vitor@hotmail.com>
2020-09-09 14:35:33 -0300

    Feat project structure

---
+++ lib/financing_system.ex
@@ -0,0 +1,18 @@
+defmodule FinancingSystem do
+  @moduledoc """
+  Documentation for `FinancingSystem`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FinancingSystem.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/financing_system_test.exs
@@ -0,0 +1,8 @@
+defmodule FinancingSystemTest do
+  use ExUnit.Case
+  doctest FinancingSystem
+
+  test "greets the world" do
+    assert FinancingSystem.hello() == :world
+  end
+end
