9b24db4475fb502d7a79bfd8afb3ea75b2063f0b
OsmarVitor <perez_vitor@hotmail.com>
2020-09-09 16:48:38 -0300

    Feat: implementing money transfer between users

--- test/financing_system_test.exs
+++ test/financing_system_test.exs
@@ -26,4 +26,16 @@ defmodule FinancingSystemTest do
     assert FinancingSystem.payment(user_test, -200) == "invalid operation!"
   end
 
+  test "transfer 100 of user1 to user2" do
+    user1 = %{:uuid => "7cec4d2e-5b6d-487a-ab71-7c87083b7b9d", :name => "User Test 1", :balance => 1200.00, :money_pattern => 986}
+    user2 = %{:uuid => "a003be1f-a993-4743-a1e5-efd512b5c1e4", :name => "User Test 2",  :balance => 1200.00, :money_pattern => 986}
+    assert {:ok, %{:uuid => "a003be1f-a993-4743-a1e5-efd512b5c1e4", :name => "User Test 2", :balance => 1300.00, :money_pattern => 986}} == FinancingSystem.transfer_money(user1, 100, user2)
+  end
+
+  test "not transfer -10 of user1 to user2" do
+    user1 = %{:uuid => "0d29e1ab-51af-48e6-ad2d-ea380800736c", :name => "User Name", :balance => 1200.00, :money_pattern => 986}
+    user2 = %{:uuid => "8327c9be-f12e-4134-a8dc-d2f0c018a7d3", :name => "User Name", :balance => 1200.00, :money_pattern => 986}
+    assert FinancingSystem.transfer_money(user1, -10, user2) ==  "invalid operation!"
+  end
+
 end
