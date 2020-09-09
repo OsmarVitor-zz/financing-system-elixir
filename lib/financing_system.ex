e17f026eae79d0628e3ce1080cf872a7c90587a5
OsmarVitor <perez_vitor@hotmail.com>
2020-09-09 16:14:09 -0300

    Feat: function to deposit value in the user account

--- lib/financing_system.ex
+++ lib/financing_system.ex
@@ -11,8 +11,27 @@ defmodule FinancingSystem do
 
       iex> FinancingSystem.get_info_user((%{:uuid => "a003be1f-a993-4743-a1e5-efd512b5c1e4", :name => "User Name Test", :balance => 1200.000, :money_pattern => 986})
       {:ok, "User name: User Name Test, account balace: 1200"}
+  
+  get_info_user.
+
+  ## Examples
+
+    iex> FinancingSystem.deposit((%{:uuid => "a003be1f-a993-4743-a1e5-efd512b5c1e4", :name => "User Name Test", :balance => 1200.000, :money_pattern => 986}), 100)
+    {:ok, %{balance: 1300, money_pattern: 986, name: "User Name Test", uuid: "a003be1f-a993-4743-a1e5-efd512b5c1e4"}}
+
+    iex> FinancingSystem.deposit((%{:uuid => "a003be1f-a993-4743-a1e5-efd512b5c1e4", :name => "User Name Test", :balance => 1200.000, :money_pattern => 986}), -20)
+    "invalid operation!"
   """
 
   def get_info_user(user_info),
   do: {:ok, "User name: #{user_info[:name]}, account balace: #{MoneyUtils.format_money(user_info[:balance])}"}
+
+  def deposit(user, deposit_value) do
+    if deposit_value > 0 do
+      {:ok, Map.update!(user, :balance, &(MoneyUtils.format_money(&1) + MoneyUtils.format_money(deposit_value)))}
+    else
+      "invalid operation!"
+    end
+  end
+
 end
--- test/financing_system_test.exs
+++ test/financing_system_test.exs
@@ -6,4 +6,14 @@ defmodule FinancingSystemTest do
     assert {:ok, "User name: User Name, account balace: 1200"} == FinancingSystem.get_info_user(user_test)
   end
 
+  test "deposit 100 to account" do
+    user_test = %{:uuid => "9fa7802b-79f5-4588-9a5e-396068756129", :name => "User Name", :balance => 1200.000, :money_pattern => 986}
+    assert {:ok, %{:uuid => "9fa7802b-79f5-4588-9a5e-396068756129" , :name => "User Name", :balance => 1300.000, :money_pattern => 986}} == FinancingSystem.deposit(user_test, 100)
+  end
+
+  test "deposit -100 to account" do
+    model = %{:uuid => "b499de09-a081-4987-8585-c7539e8cee17", :name => "User Name", :balance => 1200.000, :money_pattern => 986}
+    assert FinancingSystem.deposit(model, -100) == "invalid operation!"
+  end
+
 end
